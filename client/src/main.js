6d536c32c1a365c2cf4c069fc7ee56602bd178da
elliotdziemiela1 <elliotanthony9@gmail.com>
2025-12-10 13:36:43 -0800

    Have morning and night forecasts for current day displaying. Adding rest of the days is almost done.

--- client/src/main.js
+++ client/src/main.js
@@ -1,18 +1,48 @@
 import { getUpcomingWeather } from "../utils/services";
 
+const zoneIDChicago = "ILZ104"
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDate = new Date;
+const currentDayOfWeek = currentDate.getUTCDay();
+
+
 async function main() {
-  const nextForecast = await getUpcomingWeather(1);
-  const forecastNameElmt = document.querySelector(".nextForecastName");
-  const forecastBodyElmt = document.querySelector(".nextForecastBody");
-
-  console.log(nextForecast);
-
-  forecastNameElmt.innerHTML = nextForecast.name;
-  forecastBodyElmt.innerHTML = nextForecast.detailedForecast;
-
-  for (let i = 1; i <= 7; i++){
-    
+  // fetch the forecasts for the next 7 days
+  const nextForecasts = await getUpcomingWeather(zoneIDChicago, "land", 1, 14);
+  const forecastNameElmt = document.querySelector("#next-forecast-name");
+  const forecastBodyElmt = document.querySelector("#next-forecast-body");
+  
+  console.log(nextForecasts);
+  
+  forecastNameElmt.innerHTML = nextForecasts[0].name;
+  forecastBodyElmt.innerHTML = nextForecasts[0].detailedForecast;
+  
+  // get the date for tomorrow
+  let tomorrowDate = currentDate;
+  tomorrowDate.setDate(tomorrowDate.setDate(tomorrowDate.getDate()+1))
+  
+  // due to the lack of strict formatting for the NWS api, we must determine at what index in
+  // the nextForcasts that tomorrow's forecasts start, since we don't know if either 1 entry or 2 entries
+  // belong to the current day.
+  let tomorrowIdx = 1;
+  for (tomorrowIdx = 1; tomorrowIdx <= 2; tomorrowIdx++){
+    if (nextForecasts[tomorrowIdx].name == tomorrowDate.getDay())
+      break;
   }
+
+  // if api gives only a night forecast for today
+  if (tomorrowIdx == 1){
+    document.querySelector("#day-1 .night-forecast h4").innerHTML = nextForecasts[0].name;
+    document.querySelector("#day-1 .night-forecast p").innerHTML = nextForecasts[0].detailedForecast;
+  } else { // else api gives a morning and a night forecast for today
+    document.querySelector("#day-1 .morning-forecast h4").innerHTML = nextForecasts[0].name;
+    document.querySelector("#day-1 .morning-forecast p").innerHTML = nextForecasts[0].detailedForecast;
+    document.querySelector("#day-1 .night-forecast h4").innerHTML = nextForecasts[1].name;
+    document.querySelector("#day-1 .night-forecast p").innerHTML = nextForecasts[1].detailedForecast;
+  }
+
+  // now fill in the rest of the days for the next week
+  
 }
 
 main();
--- client/utils/services.js
+++ client/utils/services.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 
 const baseApi = "https://api.weather.gov"
-const NWSForcastZoneIDChicago = "ILZ104"
-const zoneType = "land"
 
-export async function getUpcomingWeather(num){
-  let response = await axios.get(baseApi + "/zones/" + zoneType + "/" + NWSForcastZoneIDChicago + "/forecast")
-  let weather = ""
-  response.data.properties.periods.forEach((forecast) => {if (forecast.number == num) weather = forecast;});
-  // console.log(weather)
-  return weather;
+// returns an array of objects representing the upcoming forecasts in the range of first to last (inclusive).
+export async function getUpcomingWeather(zoneID, zoneType, first, last){
+  let response = await axios.get(baseApi + "/zones/" + zoneType + "/" + zoneID + "/forecast")
+  let forecasts = []
+  response.data.properties.periods.forEach((forecast) => {
+    if (forecast.number >= first && forecast.number <= last) 
+      forecasts[forecast.number - 1] = forecast;
+  });
+  // console.log(`Forecasts: ${forecasts}`)
+  return forecasts;
 }
