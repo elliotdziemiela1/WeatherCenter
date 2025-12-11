89090e4b516630556bdded539292a067b09fe00b
elliotdziemiela1 <elliotanthony9@gmail.com>
2025-12-11 11:32:38 -0800

    created functions for fetching hourly and weekly weather using coordinates

--- client/src/main.js
+++ client/src/main.js
@@ -1,40 +1,44 @@
-import { getUpcomingWeather } from "../utils/services";
+import { getWeekForecast, getUpcomingWeather } from "../utils/services";
 
 const zoneIDChicago = "ILZ104"
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDate = new Date;
 const currentDayOfWeekIdx = currentDate.getUTCDay();
+const ChicagoLatitude = 41.8832;
+const ChicagoLongitude = -87.6324;
 
 
 async function main() {
   // fetch the forecasts for the next 7 days
-  const nextForecasts = await getUpcomingWeather(zoneIDChicago, "land", 1, 14);
+  // const twelveHourForecasts = await getUpcomingWeather(zoneIDChicago, "land", 1, 14);
+  const twelveHourForecasts = await getWeekForecast(ChicagoLatitude, ChicagoLongitude, 1, 14);
+  console.log(twelveHourForecasts)
+
   const forecastNameElmt = document.querySelector("#next-forecast-name");
   const forecastBodyElmt = document.querySelector("#next-forecast-body");
-  
-  console.log(nextForecasts);
-  
-  forecastNameElmt.innerHTML = nextForecasts[0].name;
-  forecastBodyElmt.innerHTML = nextForecasts[0].detailedForecast;
+    
+  forecastNameElmt.innerHTML = twelveHourForecasts[0].name;
+  forecastBodyElmt.innerHTML = twelveHourForecasts[0].detailedForecast;
   
   // due to the lack of strict formatting for the NWS api, the current day can have either two forecasts (today, tonight)
-  // or one forecast (tonight) in the nextForecasts array. If it's only 1 forecast, we will set an offset of -1 to use when 
+  // or one forecast (tonight) in the twelveHourForecasts array. If it's only 1 forecast, we will set an offset of -1 to use when 
   // iterating through the forecasts for the days proceeding the current day to accurately map the forecasts to the days.
   let forecastOffset = 0;
-  if (nextForecasts[1].name == daysOfWeek[(currentDayOfWeekIdx+1) % 7]) // if the second forecast is for tomorrow
+  if (twelveHourForecasts[1].name == daysOfWeek[(currentDayOfWeekIdx+1) % 7]) // if the second forecast is for tomorrow
     forecastOffset = -1;
       
+  console.log(forecastOffset);
 
   // if api gives only a night forecast for today
   if (forecastOffset == -1){
-    document.querySelector("#day-1 .night-forecast h4").innerHTML = nextForecasts[0].name;
-    document.querySelector("#day-1 .night-forecast p").innerHTML = nextForecasts[0].detailedForecast;
+    document.querySelector("#day-1 .night-forecast h4").innerHTML = twelveHourForecasts[0].name;
+    document.querySelector("#day-1 .night-forecast p").innerHTML = twelveHourForecasts[0].detailedForecast;
     document.querySelector("#day-1 .day-title").innerHTML = daysOfWeek[currentDayOfWeekIdx];
   } else { // else api gives a morning and a night forecast for today
-    document.querySelector("#day-1 .morning-forecast h4").innerHTML = nextForecasts[0].name;
-    document.querySelector("#day-1 .morning-forecast p").innerHTML = nextForecasts[0].detailedForecast;
-    document.querySelector("#day-1 .night-forecast h4").innerHTML = nextForecasts[1].name;
-    document.querySelector("#day-1 .night-forecast p").innerHTML = nextForecasts[1].detailedForecast;
+    document.querySelector("#day-1 .morning-forecast h4").innerHTML = twelveHourForecasts[0].name;
+    document.querySelector("#day-1 .morning-forecast p").innerHTML = twelveHourForecasts[0].detailedForecast;
+    document.querySelector("#day-1 .night-forecast h4").innerHTML = twelveHourForecasts[1].name;
+    document.querySelector("#day-1 .night-forecast p").innerHTML = twelveHourForecasts[1].detailedForecast;
     document.querySelector("#day-1 .day-title").innerHTML = daysOfWeek[currentDayOfWeekIdx];
 
   }
@@ -42,11 +46,17 @@ async function main() {
   // forecastOffset is either 0 or -1
   // now fill in the rest of the days for the next week
   for (let i = 1; i < 7; i++){
-    document.querySelector(`#day-${i+1} .morning-forecast h4`).innerHTML = nextForecasts[(i*2) + forecastOffset].name;
-    document.querySelector(`#day-${i+1} .morning-forecast p`).innerHTML = nextForecasts[(i*2) + forecastOffset].detailedForecast;
-    document.querySelector(`#day-${i+1} .night-forecast h4`).innerHTML = nextForecasts[(i*2) + forecastOffset + 1].name;
-    document.querySelector(`#day-${i+1} .night-forecast p`).innerHTML = nextForecasts[(i*2) + forecastOffset + 1].detailedForecast;
-    document.querySelector(`#day-${i+1} .day-title`).innerHTML = daysOfWeek[(currentDayOfWeekIdx+i) % 7];
+    document.querySelector(`#day-${i+1} .day-title`).innerHTML = daysOfWeek[(currentDayOfWeekIdx+i) % 7]; // set day of week name
+
+    // set morning forecast
+    document.querySelector(`#day-${i+1} .morning-forecast h4`).innerHTML = twelveHourForecasts[(i*2) + forecastOffset].name;
+    document.querySelector(`#day-${i+1} .morning-forecast p`).innerHTML = twelveHourForecasts[(i*2) + forecastOffset].detailedForecast;
+
+    // set night forecast
+    if (twelveHourForecasts[(i*2) + forecastOffset + 1]){
+      document.querySelector(`#day-${i+1} .night-forecast h4`).innerHTML = twelveHourForecasts[(i*2) + forecastOffset + 1].name;
+      document.querySelector(`#day-${i+1} .night-forecast p`).innerHTML = twelveHourForecasts[(i*2) + forecastOffset + 1].detailedForecast;
+    }
   }
 
 
--- client/utils/services.js
+++ client/utils/services.js
@@ -2,14 +2,46 @@ import axios from "axios"
 
 const baseApi = "https://api.weather.gov"
 
-// returns an array of objects representing the upcoming forecasts in the range of first to last (inclusive).
+// returns an array of objects representing the upcoming 12 hour forecasts in the range of first to last (inclusive).
 export async function getUpcomingWeather(zoneID, zoneType, first, last){
   let response = await axios.get(baseApi + "/zones/" + zoneType + "/" + zoneID + "/forecast")
   let forecasts = []
   response.data.properties.periods.forEach((forecast) => {
-    if (forecast.number >= first && forecast.number <= last) 
+    if (forecast.number >= first && forecast.number <= last){
       forecasts[forecast.number - 1] = forecast;
+    }
   });
   // console.log(`Forecasts: ${forecasts}`)
   return forecasts;
+}
+
+// returns an array of objects representing the upcoming hourly forecasts in the range of first to last (inclusive)
+export async function getHourlyForecast(latitude, longitude, first, last){
+  const pointsResponse = await axios.get(baseApi + "/points/" + latitude + "," + longitude);
+  const hourlyURL = pointsResponse.data.forecastHourly;
+  const response = await axios.get(hourlyURL);
+  let hourlyForecasts = [];
+  
+  response.data.properties.periods.forEach((period) => {
+    if (period.number >= first && period.number <= last){
+      hourlyForecasts[period.number - 1] = period;
+    }
+  })
+
+  return hourlyForecasts;
+}
+
+export async function getWeekForecast(latitude, longitude, first, last){
+  const pointsResponse = await axios.get(baseApi + "/points/" + latitude + "," + longitude);
+  const forecastURL = pointsResponse.data.properties.forecast;
+  const response = await axios.get(forecastURL);
+  let twelveHourForecasts = [];
+  
+  response.data.properties.periods.forEach((period) => {
+    if (period.number >= first && period.number <= last){
+      twelveHourForecasts[period.number - 1] = period;
+    }
+  })
+
+  return twelveHourForecasts;
 }
