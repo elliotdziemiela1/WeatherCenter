22129e61fae272055822710b3049627a906d6ae2
elliotdziemiela1 <elliotanthony9@gmail.com>
2025-12-11 09:31:04 -0800

    shows 7 day forecast

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import { getUpcomingWeather } from "../utils/services";
 const zoneIDChicago = "ILZ104"
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDate = new Date;
-const currentDayOfWeek = currentDate.getUTCDay();
+const currentDayOfWeekIdx = currentDate.getUTCDay();
 
 
 async function main() {
@@ -17,32 +17,39 @@ async function main() {
   forecastNameElmt.innerHTML = nextForecasts[0].name;
   forecastBodyElmt.innerHTML = nextForecasts[0].detailedForecast;
   
-  // get the date for tomorrow
-  let tomorrowDate = currentDate;
-  tomorrowDate.setDate(tomorrowDate.setDate(tomorrowDate.getDate()+1))
-  
-  // due to the lack of strict formatting for the NWS api, we must determine at what index in
-  // the nextForcasts that tomorrow's forecasts start, since we don't know if either 1 entry or 2 entries
-  // belong to the current day.
-  let tomorrowIdx = 1;
-  for (tomorrowIdx = 1; tomorrowIdx <= 2; tomorrowIdx++){
-    if (nextForecasts[tomorrowIdx].name == tomorrowDate.getDay())
-      break;
-  }
+  // due to the lack of strict formatting for the NWS api, the current day can have either two forecasts (today, tonight)
+  // or one forecast (tonight) in the nextForecasts array. If it's only 1 forecast, we will set an offset of -1 to use when 
+  // iterating through the forecasts for the days proceeding the current day to accurately map the forecasts to the days.
+  let forecastOffset = 0;
+  if (nextForecasts[1].name == daysOfWeek[(currentDayOfWeekIdx+1) % 7]) // if the second forecast is for tomorrow
+    forecastOffset = -1;
+      
 
   // if api gives only a night forecast for today
-  if (tomorrowIdx == 1){
+  if (forecastOffset == -1){
     document.querySelector("#day-1 .night-forecast h4").innerHTML = nextForecasts[0].name;
     document.querySelector("#day-1 .night-forecast p").innerHTML = nextForecasts[0].detailedForecast;
+    document.querySelector("#day-1 .day-title").innerHTML = daysOfWeek[currentDayOfWeekIdx];
   } else { // else api gives a morning and a night forecast for today
     document.querySelector("#day-1 .morning-forecast h4").innerHTML = nextForecasts[0].name;
     document.querySelector("#day-1 .morning-forecast p").innerHTML = nextForecasts[0].detailedForecast;
     document.querySelector("#day-1 .night-forecast h4").innerHTML = nextForecasts[1].name;
     document.querySelector("#day-1 .night-forecast p").innerHTML = nextForecasts[1].detailedForecast;
+    document.querySelector("#day-1 .day-title").innerHTML = daysOfWeek[currentDayOfWeekIdx];
+
   }
 
+  // forecastOffset is either 0 or -1
   // now fill in the rest of the days for the next week
-  
+  for (let i = 1; i < 7; i++){
+    document.querySelector(`#day-${i+1} .morning-forecast h4`).innerHTML = nextForecasts[(i*2) + forecastOffset].name;
+    document.querySelector(`#day-${i+1} .morning-forecast p`).innerHTML = nextForecasts[(i*2) + forecastOffset].detailedForecast;
+    document.querySelector(`#day-${i+1} .night-forecast h4`).innerHTML = nextForecasts[(i*2) + forecastOffset + 1].name;
+    document.querySelector(`#day-${i+1} .night-forecast p`).innerHTML = nextForecasts[(i*2) + forecastOffset + 1].detailedForecast;
+    document.querySelector(`#day-${i+1} .day-title`).innerHTML = daysOfWeek[(currentDayOfWeekIdx+i) % 7];
+  }
+
+
 }
 
 main();
